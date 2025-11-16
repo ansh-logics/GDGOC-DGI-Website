a3ad086bbe55703f84ec813694e46d3926f746db
ansh-logics <anshbhatt140@icloud.com>
2025-11-16 14:53:57 +0530

    working

--- frontend/app/api/controllers/getAllEvents.ts
+++ frontend/app/api/controllers/getAllEvents.ts
@@ -5,6 +5,7 @@ export default async function getAllEvents(){
         throw new Error("No event data found in cache");
     }else{
         let data = await match.json();
+        console.log(data);
         console.log(data['id']);
         return data;
     }
