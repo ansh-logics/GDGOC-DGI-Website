d4d96e613536a2fd5ac5aa18428560852bee03b3
ansh-logics <anshbhatt140@icloud.com>
2026-02-10 11:13:42 +0530

    update: initated backend

---
+++ backend/connectDb.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+let MONGO_URL = process.env.MONGO_CONNECTION
+
+export async function connectdb(){
+    try{
+        await mongoose.connect(MONGO_URL);
+        return "Db connected Successfully"
+    }catch(err){
+        throw new Error("Db not connected check uri");
+    }
+}
---
+++ backend/index.js
@@ -0,0 +1,24 @@
+import express from "express"
+import dotenv from 'dotenv'
+import { connectdb } from "./connectDb.js";
+
+dotenv.config();
+try {
+    let message = await connectdb();
+    console.log(message);
+} catch (error) {
+  console.log(error.messsage);
+}
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/test", (req, res)=>{
+    res.send("This hits the backend and it's running");
+});
+
+app.listen(PORT,()=>{
+    console.log("server is running on PORT = ", PORT);
+});
